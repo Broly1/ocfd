d312242f27d44aa3013071c718c2aea16019ff7c
Steven Cole <67975160+ZackeryCole@users.noreply.github.com>
2022-01-28 18:15:09 -0500

    ocflashdrive: add alpine linux support

--- ocflashdrive.sh
+++ ocflashdrive.sh
@@ -36,6 +36,8 @@ EOF
         dnf install -y wget curl p7zip-plugins
         elif [[ -f /etc/arch-release ]]; then
         pacman -Sy --noconfirm --needed wget curl p7zip
+        elif [[ -f /etc/alpine-release ]]; then
+        apk add wget curl p7zip sgdisk sgdisk
     else
         printf "Your distro is not supported!\n"
         exit 1
