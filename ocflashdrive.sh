d2602b60e6a61b73de73a8714fdbc5869f43c25a
broly <jpegrande@gmail.com>
2022-12-25 15:06:40 -0500

    ocflashdrive: clean up script and fix Path according to latest oc release

--- ocflashdrive.sh
+++ ocflashdrive.sh
@@ -1,59 +1,56 @@
 #!/bin/bash
-# Autor: Broly
+# Author: Broly
 # License: GNU General Public License v3.0
 # https://www.gnu.org/licenses/gpl-3.0.txt
-# This script is inteded to create an opencore usb-installer on linux
+# This script is intended to create an OpenCore USB-installer on Linux.
 
+# This function clears the screen and checks if the user is root. If not, it will execute the script with sudo.
 welcome(){
-clear
-	cat << "EOF"
-################################
-#    WELCOME TO OCFLASHDRIVE   #
-################################
-
-Please enter your password!
-
-EOF
-
-[[ "$(whoami)" != "root" ]] && exec sudo -- "$0" "$@"
-set -e
+    clear
+    printf "Welcome to the OpenCore USB-installer script.\n\n"
+    [[ "$(whoami)" != "root" ]] && exec sudo -- "$0" "$@"
+    set -e
 }
 
-ImportantTools(){
+# This function gets the USB drive selected by the user.
+getthedrive(){
     clear
-	cat << "EOF"
-################################
-#    INSTALLING DEPENDENCIES   #
-################################
+    printf "Please select the USB drive to use:\n\n"
+    readarray -t lines < <((lsblk -p -no name,size,MODEL,VENDOR,TRAN | grep "usb"))
+    select choice in "${lines[@]}"; do
+        [[ -n "$choice" ]] || { printf ">>> Invalid Selection!\n" >&2; continue; }
+        break
+    done
+    read -r drive _ <<<"$choice"
+    if [[ -z "$choice" ]]; then
+        printf "Please insert the USB Drive and try again.\n"
+        exit 1
+    fi
+}
 
-Installing wget curl p7zip!
-
-EOF
+# This function installs the necessary dependencies for the script to run.
+dependencies(){
+    clear
+    printf "Installing dependencies...\n\n"
     sleep 2s
     if [[ -f /etc/debian_version ]]; then
         apt install -y wget curl p7zip-full
-        elif [[ -f /etc/fedora-release ]]; then
+    elif [[ -f /etc/fedora-release ]]; then
         dnf install -y wget curl p7zip-plugins
-        elif [[ -f /etc/arch-release ]]; then
+    elif [[ -f /etc/arch-release ]]; then
         pacman -Sy --noconfirm --needed wget curl p7zip
-        elif [[ -f /etc/alpine-release ]]; then
-        apk add wget curl p7zip sgdisk sgdisk
+    elif [[ -f /etc/alpine-release ]]; then
+        apk add wget curl p7zip
     else
         printf "Your distro is not supported!\n"
         exit 1
     fi
 }
 
-partformat(){
+# This function formats the USB drive.
+formater(){
     clear
-	cat << "EOF"
-###############################
-#    PARTITIONING THE DRIVE   #
-###############################
-
-Formating and partitioning the drive with wipefs and sgdisk!
-
-EOF
+    printf "Formatting the USB drive...\n\n"
     umount "$drive"* || :
     sleep 2s
     wipefs -af "$drive"
@@ -62,101 +59,70 @@ EOF
     sleep 2s
 }
 
+# This function prompts the user if they want to continue with the formatting and installation of dependencies.
 formating(){
-while true; do
-read -r -p "$(printf %s "Drive ""$drive"" will be erased, wget, curl and p7zip will be installed do you wish to continue (y/n)? ")" yn
-	case $yn in
-		[Yy]*)
-			ImportantTools "$@"; partformat "$@"
-			break
-			;;
-		[Nn]*) exit ;;
-		*) printf "Please answer yes or no.\n" ;;
-	esac
-done
+    while true; do
+        read -r -p "$(printf %s "Drive ""$drive"" will be erased, wget, curl, and p7zip will be installed. Do you wish to continue (y/n)? ")" yn
+        case $yn in
+            [Yy]*)
+                dependencies "$@"; formater "$@"
+                break
+                ;;
+            [Nn]*) 
+                printf "Exiting the script...\n"
+                exit 
+                ;;
+            *) 
+                printf "Please answer yes or no.\n" 
+                ;;
+        esac
+    done
 }
 
-extractor(){
-    clear
-	cat << "EOF"
-#############################
-#    EXTRACTING DMG FILE    #
-#############################
-
-Extracting BaseSystem.dmg with p7zip!
-
-EOF
-    
-    FILE=(*.dmg)
-    if [[ -f "${FILE[*]}" ]]; then
-        rm -rf -- *.hfs
-        7z e -tdmg -- *.dmg -- *.hfs
+# This function extracts the macOS recovery image from the downloaded DMG file.
+extractor() {
+    printf "Extracting macOS recovery image...\n"
+    FILE=(com.apple.recovery.boot/*.dmg)
+    if [ -n "$FILE" ]; then
+        7z e -tdmg "${FILE[*]}" -ocom.apple.recovery.boot/ -- *.hfs
     else
-        printf "Please Download the macOS Recovery with macrecovery!\n"
+        printf "Please download the macOS Recovery with macrecovery!\n"
         exit 1
     fi
 }
 
+# Burn the macOS recovery image to the target drive
 burning(){
     clear
-	cat << "EOF"
-####################################
-#    COPYING IMAGE TO THE DRIVE    #
-####################################
-
-Copying image to the flash drive with dd command!
-
-EOF
-    myhfs=$(ls *.hfs)
+    myhfs=$(ls com.apple.recovery.boot/*.hfs)
+    printf "Burning the macOS recovery image to $drive...\n"
     dd bs=8M if="$myhfs" of="$drive"2 status=progress oflag=sync
-    rm -rf -- *.hfs
+    rm -rf com.apple.recovery.boot/*.hfs
     umount "$drive"?* || :
     sleep 3s
+    printf "The macOS recovery image has been burned to $drive!\n"
 }
 
+# Install OpenCore to the target drive
 InstallOC(){
     clear
-	cat << "EOF"
-#################################
-#    INSTALLING OPENCORE EFI    #
-#################################
-EOF
+    printf "Installing OpenCore to $drive...\n"
     mkfs.fat -F32 -n OPENCORE "$drive"1
     mount -t vfat "$drive"1 /mnt/ -o rw,umask=000; sleep 3s
     cp -r ../../X64/EFI/ /mnt/
     cp -r ../../Docs/Sample.plist /mnt/EFI/OC/
-    printf "Installation finished, open /mnt and edit oc for your machine!!\n"
+    printf "OpenCore has been installed to $drive! Please open /mnt and edit OC for your machine!!\n"
 }
 
-getthedrive(){
-clear
-cat << "EOF"
-################################################
-#  WARNING: THE SELECTED DRIVE WILL BE ERASED  #
-################################################
-
-Please select the usb-drive!
-
-EOF
-readarray -t lines < <((lsblk -p -no name,size,MODEL,VENDOR,TRAN | grep "usb"))
-select choice in "${lines[@]}"; do
-    [[ -n "$choice" ]] || { printf ">>> Invalid Selection!\n" >&2; continue; }
-    break
-done
-read -r drive _ <<<"$choice"
-if [[ -z "$choice" ]]; then
-	printf "Please insert the USB Drive and try again.\n"
-	exit 1
-fi
-}
-
+# Main function that runs all the sub-functions
 main() {
-	welcome "$@"
-	getthedrive "$@"
-	formating "$@"
+    welcome "$@"
+    getthedrive "$@"
+    formating "$@"
     extractor "$@"
-	burning "$@"
+    burning "$@"
     InstallOC "$@"
 }
 
+# Run the main function with all the arguments passed to the script
 main "$@"
